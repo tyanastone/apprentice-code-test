3b9ff61aa919c81c140f7684084866eb32c1fe8f
Tyana Stone <tyanastone@Moniques-MacBook.local>
2019-05-15 20:09:09 -0400

    finished index functions

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 const add = (x, y) => {
   return x + y;
 };
+console.log(add(10,20));
 
 const subtract = (x, y) => {
   return x - y;
 };
+console.log(subtract(4,3));
 
 const divide = (x, y) => {
   return x / y;
 };
+console.log(divide(10,5));
 
 const multiply = (x, y) => {
   return x * y;
 };
+console.log(multiply(2,3));
 
 const areSameLength = (str1, str2) => {
   return str1.length === str2.length;
 };
+console.log(areSameLength('cake','blue'));
 
 const areEqual = (x, y) => {
   return x === y;
 };
+console.log(areEqual(4,4));
 
 const isEven = num => {
   if (num % 2 === 0) {
@@ -28,6 +34,7 @@ const isEven = num => {
   }
   return false;
 };
+console.log(isEven(4));
 
 const isOdd = num => {
   if (num % 2 === 0) {
@@ -35,10 +42,12 @@ const isOdd = num => {
   }
   return true;
 };
+console.log(isOdd(2));
 
 const roundNumber = num => {
   return Math.round(num);
 };
+console.log(roundNumber(5.95));
 
 export {
   add,
